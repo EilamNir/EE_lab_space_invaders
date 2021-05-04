9ff5415e9c5f6c4e797adcc359dcfa9a87a1dcec
EilamNir <nir1eilam@gmail.com>
2021-05-04 13:18:54 +0300

    integrate player into the top module
    not compiling yet

--- controller/space_invaders_TOP.sv
+++ controller/space_invaders_TOP.sv
@@ -28,8 +28,24 @@ module space_invaders_TOP
     logic [PIXEL_WIDTH - 1:0] pixelY;
     logic [RGB_WIDTH - 1:0] background_RGB;
 
+    logic signed [10:0] topLeftX;
+    logic signed [10:0] topLeftY;
+
     clock_divider clock_div_inst (.refclk(CLOCK_50), .rst(~resetN), .outclk_0(clk));
-    player player_inst ();
+
+    player player_inst (
+    .clk            (clk),
+    .resetN         (resetN),
+    .PS2_CLK        (PS2_CLK),
+    .PS2_DAT        (PS2_DAT),
+    .startOfFrame   (startOfFrame),
+    .PixelX         (pixelX),
+    .PixelY         (pixelY),
+    .topLeftX       (topLeftX),
+    .topLeftY       (topLeftY),
+    .playerDR       (playerDR),
+    .playerRGB      (playerRGB));
+
     monsters monsters_inst ();
     hit_detection hit_detection_inst ();
     missiles missiles_inst ();
@@ -45,9 +61,9 @@ module space_invaders_TOP
 
     // TODO: Remove all of this, it is here only as a placeholder until there is something to draw
     logic [0:1] [7:0] obj_RGB;
-    assign obj_RGB = {8'b11100000, 8'b00000011};
+    assign obj_RGB = {playerRGB, 8'b00000011};
     logic [0:1] draw_requests;
-    assign draw_requests = {1'b0, 1'b0};
+    assign draw_requests = {playerDR, 1'b0};
 
 
     video_unit video_unit_inst (
--- player/player.sv
+++ player/player.sv
@@ -1,116 +1,115 @@
 
 
-module  player ( 
+module  player (
+    input logic clk,
+    input logic resetN,
+    input logic PS2_CLK,
+    input logic PS2_DAT,
+    input logic startOfFrame,
+    input logic [10:0]PixelX,
+    input logic [10:0]PixelY,
 
-	input logic clk,
-	input logic resetN,
-	input logic PS2_CLK,
-	input logic PS2_DAT,
-	input logic startOfFrame,
-	input logic [10:0]PixelX,	
-	input logic [10:0]PixelY,	
-
-	output logic signed [10:0] topLeftX,
-	output logic signed [10:0] topLeftY,
-	output logic playerDR,
-	output logic [7:0] playerRGB	
+    output logic signed [10:0] topLeftX,
+    output logic signed [10:0] topLeftY,
+    output logic playerDR,
+    output logic [7:0] playerRGB
 );
 
-	logic keyCode[8:0];
-	logic make;
-	logic brake;
-	
-	keyboard_interface kbd_inst(
-		.clk(clk),
-		.resetN(resetN),
-		.PS2_CLK(PS2_CLK),
-		.PS2_DAT(PS2_DAT),
-		.keyCode(keyCode),
-		.make(make),
-		.brake(brake)
-		);
-	
-	parameter UP    = 9'h075; // digit 8
-	parameter DOWN  = 9'h073; // digit 5
-	parameter RIGHT = 9'h074; // digit 6
-	parameter LEFT  = 9'h06B; // digit 4
-	
-	logic upIsPress;
-	keyToggle_decoder #(.KEY_VALUE(UP)) control_up_inst (
-		.clk(clk),
-		.resetN(resetN),
-		.keyCode(keyCode),
-		.make(make),
-		.brakee(brake),
-		.keyIsPressed(upIsPress)
-		);
-	
-	logic downIsPress;
-	keyToggle_decoder #(.KEY_VALUE(DOWN)) control_down_inst (
-		.clk(clk),
-		.resetN(resetN),
-		.keyCode(keyCode),
-		.make(make),
-		.brakee(brake),
-		.keyIsPressed(downIsPress)
-		);
-	logic RightIsPress;
-	keyToggle_decoder #(.KEY_VALUE(RIGHT)) control_right_inst (
-		.clk(clk),
-		.resetN(resetN),
-		.keyCode(keyCode),
-		.make(make),
-		.brakee(brake),
-		.keyIsPressed(RightIsPress)
-		);	
-	
-	logic LeftIsPress;
-	keyToggle_decoder #(.KEY_VALUE(LEFT)) control_left_inst (
-		.clk(clk),
-		.resetN(resetN),
-		.keyCode(keyCode),
-		.make(make),
-		.brakee(brake),
-		.keyIsPressed(LeftIsPress)
-		);
-	
-	player_move player_move_inst(
-		.clk(clk),         
-		.resetN(resetN),      
-		.startOfFrame(startOfFrame), 
-		.move_left(LeftIsPress),   
-		.move_right(RightIsPress),  
-		.move_up(upIsPress),    
-		.move_down(downIsPress),   
-		.topLeftX(topLeftX),
-		.topLeftY(topLeftY)
-		);		
-		
-		
-	square_object square_object_inst(
-		.clk(clk),         
-		.resetN(resetN), 
-		.pixelX(pixelX),
-		.pixelY(pixelY),
-		.topLeftX(topLeftX), 
-		.topLeftY(topLeftY),  
-		.offsetX(offsetX),
-		.offsetY(offsetY),
-		.drawingRequest(drawingRequest),
-		.RGBout(RGBout)
-		);
-		
-		
-	spaceShipBitMap spaceShipBitMap_inst(
-		.clk(clk),         
-		.resetN(resetN),  
-		.offsetX(offsetX),
-		.offsetY(offsetY), 
-		.InsideRectangle(drawingRequest), 
-		.drawingRequest(playerDR),  
-		.RGBout(playerRGB)        
-	);
-		
-		
-	
+    logic keyCode[8:0];
+    logic make;
+    logic brake;
+
+    keyboard_interface kbd_inst(
+        .clk(clk),
+        .resetN(resetN),
+        .PS2_CLK(PS2_CLK),
+        .PS2_DAT(PS2_DAT),
+        .keyCode(keyCode),
+        .make(make),
+        .brake(brake)
+        );
+
+    parameter UP    = 9'h075; // digit 8
+    parameter DOWN  = 9'h073; // digit 5
+    parameter RIGHT = 9'h074; // digit 6
+    parameter LEFT  = 9'h06B; // digit 4
+
+    logic upIsPress;
+    keyToggle_decoder #(.KEY_VALUE(UP)) control_up_inst (
+        .clk(clk),
+        .resetN(resetN),
+        .keyCode(keyCode),
+        .make(make),
+        .brakee(brake),
+        .keyIsPressed(upIsPress)
+        );
+
+    logic downIsPress;
+    keyToggle_decoder #(.KEY_VALUE(DOWN)) control_down_inst (
+        .clk(clk),
+        .resetN(resetN),
+        .keyCode(keyCode),
+        .make(make),
+        .brakee(brake),
+        .keyIsPressed(downIsPress)
+        );
+    logic RightIsPress;
+    keyToggle_decoder #(.KEY_VALUE(RIGHT)) control_right_inst (
+        .clk(clk),
+        .resetN(resetN),
+        .keyCode(keyCode),
+        .make(make),
+        .brakee(brake),
+        .keyIsPressed(RightIsPress)
+        );
+
+    logic LeftIsPress;
+    keyToggle_decoder #(.KEY_VALUE(LEFT)) control_left_inst (
+        .clk(clk),
+        .resetN(resetN),
+        .keyCode(keyCode),
+        .make(make),
+        .brakee(brake),
+        .keyIsPressed(LeftIsPress)
+        );
+
+    player_move player_move_inst(
+        .clk(clk),
+        .resetN(resetN),
+        .startOfFrame(startOfFrame),
+        .move_left(LeftIsPress),
+        .move_right(RightIsPress),
+        .move_up(upIsPress),
+        .move_down(downIsPress),
+        .topLeftX(topLeftX),
+        .topLeftY(topLeftY)
+        );
+
+
+    square_object square_object_inst(
+        .clk(clk),
+        .resetN(resetN),
+        .pixelX(pixelX),
+        .pixelY(pixelY),
+        .topLeftX(topLeftX),
+        .topLeftY(topLeftY),
+        .offsetX(offsetX),
+        .offsetY(offsetY),
+        .drawingRequest(drawingRequest),
+        .RGBout(RGBout)
+        );
+
+
+    spaceShipBitMap spaceShipBitMap_inst(
+        .clk(clk),
+        .resetN(resetN),
+        .offsetX(offsetX),
+        .offsetY(offsetY),
+        .InsideRectangle(drawingRequest),
+        .drawingRequest(playerDR),
+        .RGBout(playerRGB)
+    );
+
+
+
 endmodule
