f52586431eb99b99c3c2104ed1f7afa03d9fb424
EilamNir <nir1eilam@gmail.com>
2021-05-26 03:13:10 +0300

    remove redundent code checking for overlaping in monsters

--- monsters/check_overlap.sv
+++ monsters/check_overlap.sv
@@ -6,24 +6,27 @@ module check_overlap(
     input logic [OBJECT_AMOUNT - 1:0] draw_request,
     input logic [OBJECT_AMOUNT - 1:0] object_deactivated,
 
-    output logic overlap
+    output logic overlap,
+    output logic any_DR,
+    output logic [OBJECT_AMOUNT_WIDTH - 1:0] first_object_index
 );
     parameter unsigned OBJECT_AMOUNT_WIDTH = 4;
     parameter logic unsigned [OBJECT_AMOUNT_WIDTH - 1:0] OBJECT_AMOUNT = 3;
 
-    logic [OBJECT_AMOUNT - 1:0] first_object_index;
-    logic [OBJECT_AMOUNT - 1:0] last_object_index;
+    logic [OBJECT_AMOUNT_WIDTH - 1:0] last_object_index;
 
     // Find the first object that wants to be drawn in this space
     always_comb begin
         first_object_index = 0;
-        for (logic [OBJECT_AMOUNT - 1:0] j = 0; j < OBJECT_AMOUNT; j++) begin
+        any_DR = 1'b0;
+        for (logic [OBJECT_AMOUNT_WIDTH - 1:0] j = 0; j < OBJECT_AMOUNT; j++) begin
             // check that the object wants to be drawn
             if (draw_request[j] == 1'b1) begin
                 // Ignore deactivated objects
                 if (object_deactivated[j] == 1'b0) begin
                     // Save the index of the first object
                     first_object_index = j;
+                    any_DR = 1'b1;
                     // Do not continue to the next object
                     break;
                 end
@@ -34,7 +37,7 @@ module check_overlap(
     // Find the last object that wants to be drawn in this space
     always_comb begin
         last_object_index = 0;
-        for (logic [OBJECT_AMOUNT - 1:0] i = 0; i < OBJECT_AMOUNT; i++) begin
+        for (logic [OBJECT_AMOUNT_WIDTH - 1:0] i = 0; i < OBJECT_AMOUNT; i++) begin
             // check that the object wants to be drawn
             if (draw_request[i] == 1'b1) begin
                 // Ignore deactivated objects
